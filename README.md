639c722d4ca1bd0c884803e7bafb5fbe35bfc75f
rcmdnk <rcmdnk@gmail.com>
2016-03-02 18:44:03 +0900

    first import

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # octopress-post-card
 Post card like link to other page in the site.
+
+## Installation
+
+Copy `plugins/post_card.rb` to your `plugins/` directory.
+
+Copy `sass/plugins/_post-card.rb` to your `sass/plugins/` directory.
+
+## Usage
+
+    {% post_card [url/title_url/image] {title/url/filename} [comment] %}
+
+After installation, you can use `post_card` tag, like:
+
+
+    ...
+    
+    {% post_card /blog/2013/08/03/computer-windows-autohotkey/ %}
+    
+    {% post_card /blog/2013/06/10/computer-mac-keyremap4macbook-vim/ %}
+    
+    ...
+
+This will show like:
+
+![post_card](https://raw.githubusercontent.com/rcmdnk/octopress-post-card/master/post_card.jpg)
+
+An argument can be a title, an url, or a filename (like **2013-08-03-computer-windows-autohotkey.markdown**)
+of your post.
+
+An url can be w/ or w/o site url (i.e. **http://rcmdnk/github.com/blog/...** or **/blog/...**).
+
+If `url`, `title_url` or `image` is given at the first,
+it will return:
+
+* `url`: URL with link.
+* `title_url`: Title with link.
+* `image`: Image with link.
+
+Otherwise, it will return a card like link as above.
+
+The last argument of `comment` is not used, but useful
+to note what the link is.
+
+It is useful to note a title in the comment when URL is used to find a card.
+To find a card, URL is better than title, as a title can be changed.
---
+++ plugins/post_card.rb
@@ -0,0 +1,91 @@
+require "./plugins/octopress_filters"
+module Jekyll
+  module Tags
+    class PostCard < Liquid::Tag
+      include OctopressLiquidFilters
+      def initialize(tag_name, post, tokens)
+        super
+        @post = post.strip.split[0]
+        @opt = ''
+        if @post == 'url' || @post == 'title_url' || @post == 'image'
+          m = post.strip.match(/(\w*) (.*)/)
+          @opt = m[1]
+          @post = m[2]
+        end
+        if @post.split("/")[-1] == "index.html" ||
+           @post.split("/")[-1] == "index.htm"
+          @post = @post.split("/")[0..-2].join("/")
+        end
+        if ! @post.end_with?("html") && ! @post.end_with?("htm") &&
+           @post[-1] != "/"
+          @post += "/"
+        end
+      end
+
+      def render(context)
+        site = context.registers[:site]
+        if @post.start_with?(site.config['url'])
+          @post.gsub!(site.config['url'], "")
+          if @post[0] != "/"
+            @post = "/" + @post
+          end
+        end
+        if @post.start_with?(site.config['root'])
+          @post.gsub!(site.config['root'], "")
+          if @post[0] != "/"
+            @post = "/" + @post
+          end
+        end
+
+        site.posts.each do |p|
+          p p.url
+          if @post == p.title || @post == p.url ||
+             @post.split('/')[-1].split('.')[0] ==
+             p.path.split('/')[-1].split('.')[0]
+            url = p.url
+            return "<a href='#{url}'>#{url}</a>" if @opt == 'url'
+            title = p.title
+            if title =~ /(.+)\/$|^\/$/
+              title = $1
+            end
+            title.upcase! if site.config['titlecase']
+            return "<a href='#{url}'>#{title}</a>" if @opt == 'title_url'
+            img = ''
+            amazon = false
+            if !p['ogimage'].nil?
+              img = p['ogimage']
+            else
+              begin
+                img = amazon_img(p.content)
+                if img != ''
+                  img = '<div class="title-small-thumbnail">' + img + '</div>'
+                  amazon = true
+                end
+              rescue
+                img = ''
+              end
+            end
+            img = site.config['sitelogo'] if img == '' &&
+                                             site.config.key?('sitelogo')
+            unless amazon
+              thum = Jekyll::Thumbnail.new('thumbnail', "small-thumbnail #{img}", '')
+              img = '<div class="title-small-thumbnail">
+  <a href="' + url + '">' + thum.render(context) + '</a>
+</div>'
+            end
+            return img if @opt == 'image'
+            desc = ''
+            desc = '</br>' + p['description'] unless p['description'].nil?
+            card = '<ul class="post_card"><li class="index_click_box"><div class="group">' + img + \
+                   '<a class="click_box_link" href="' + url + '">' + \
+                   title + '</a>' + desc + '</div></li></ul>'
+            return card
+          end
+        end
+        "<a href='#{@post}'>#{@post}</a>"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('post_card', Jekyll::Tags::PostCard)
